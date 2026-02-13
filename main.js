6e4ecd322c9131c7a1770a95cc70d5b2e23c917f
cabatu-9e-ay2526 <cabatu.mh@obmontessori.edu.ph>
2026-02-13 11:01:09 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function area() {
+    let radius = document.getElementById("radiusval").value;
+    let result = Math.PI * radius * radius;
+    document.getElementById("areaval").value = result.toFixed(2);
+    onclick="radius"
+    document="A.tofixed(2)";
+}
